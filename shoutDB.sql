850600536cce14081da1ccfe1c838cf013292921
Oguz <Oguzhan_bozkurt@hotmail.se>
2020-03-25 07:18:45 +0100

    DB file

---
+++ shoutDB.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 25, 2020 at 07:15 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shoutit`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shouts`
+--
+
+CREATE TABLE `shouts` (
+  `id` int(11) NOT NULL,
+  `user` varchar(50) NOT NULL,
+  `message` text NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `shouts`
+--
+
+INSERT INTO `shouts` (`id`, `user`, `message`, `time`) VALUES
+(1, 'Oguz', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.', '11:25:00'),
+(2, 'Giim', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.', '11:30:00'),
+(3, 'Oguz', 'It is a long established fact that...', '11:31:00'),
+(4, 'Giim', '', '06:15:58'),
+(5, 'Oguz', '', '06:16:13'),
+(6, 'Giim', 'Are you there?', '06:19:59'),
+(7, 'Oguz', 'yES IM HERE HEHEHEHEH', '06:20:46');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `shouts`
+--
+ALTER TABLE `shouts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `shouts`
+--
+ALTER TABLE `shouts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
